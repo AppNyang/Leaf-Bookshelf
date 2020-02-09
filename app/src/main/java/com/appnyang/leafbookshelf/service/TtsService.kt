ba1ff1af094d25b156f1dba0daeb1337a64e33bb
Sangwoo Park <eicpein@gmail.com>
2020-02-10 01:26:04 +0900

    Refactoring TtsService

--- app/src/main/java/com/appnyang/leafbookshelf/service/TtsService.kt
+++ app/src/main/java/com/appnyang/leafbookshelf/service/TtsService.kt
@@ -17,6 +17,7 @@ import androidx.lifecycle.MutableLiveData
 import com.appnyang.leafbookshelf.R
 import com.appnyang.leafbookshelf.view.page.activity.PageActivity
 import java.util.*
+import kotlin.properties.Delegates
 
 /**
  * Handle TextToSpeech engine.
@@ -34,10 +35,25 @@ class TtsService : LifecycleService() {
     private val idUtterance = "LEAF_TTS"
     private lateinit var textToSpeech: TextToSpeech
 
+    private lateinit var title: CharSequence
+    private lateinit var pagedBook: LinkedList<Spanned>
+    private lateinit var currentPage: MutableLiveData<Int>
+
+    private var isTtsReady: Boolean by Delegates.observable(false) { _, _, new ->
+        // It means the service is created and engine is now initialized.
+        if (new) {
+            if (isDataSet()) {
+                readBook()
+            }
+        }
+    }
+
     var userCancelReadListener: () -> Unit = {}
 
     override fun onCreate() {
         super.onCreate()
+
+        initTtsEngine()
 
         val pendingIntent = Intent(this, PageActivity::class.java).let { notificationIntent ->
             TaskStackBuilder.create(this).run {
@@ -102,51 +118,17 @@ class TtsService : LifecycleService() {
     }
 
     /**
-     * Read the book using TTS engine.
-     *
-     * @param title
-     * @param pagedBook
-     * @param currentPage
+     * Initialize TTS Engine.
      */
-    fun read(title: CharSequence, pagedBook: LinkedList<Spanned>, currentPage: MutableLiveData<Int>) {
+    private fun initTtsEngine() {
         textToSpeech = TextToSpeech(this) { status ->
             if (status == TextToSpeech.SUCCESS) {
                 val result = textToSpeech.setLanguage(Locale.getDefault())
                 if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                     stopSelf()
                 } else {
-                    // Update notification.
-                    notification
-                        .setContentTitle(title)
-                        .setContentText(pagedBook[currentPage.value ?: 0])
-                    val notificationManager: NotificationManager =
-                        getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-                    notificationManager.notify(notificationId, notification.build())
-
-                    textToSpeech.setOnUtteranceProgressListener(object: UtteranceProgressListener() {
-                        override fun onDone(id: String?) {
-                            if (currentPage.value == null) { stopSelf() }
-
-                            currentPage.value?.let { page ->
-                                // Go to next page.
-                                if (page + 1 in pagedBook.indices) {
-                                    currentPage.postValue(currentPage.value?.plus(1))
-                                    textToSpeech.speak(pagedBook[page + 1], TextToSpeech.QUEUE_FLUSH, null, idUtterance)
-                                }
-                                else {
-                                    stopSelf()
-                                }
-                            }
-                        }
-
-                        override fun onError(id: String?) {
-                            stopSelf()
-                        }
-
-                        override fun onStart(id: String?) {}
-                    })
-
-                    textToSpeech.speak(pagedBook[currentPage.value ?: 0], TextToSpeech.QUEUE_FLUSH, null, idUtterance)
+                    // If the data is set, this leads to call readBook function below.
+                    isTtsReady = true
                 }
 
             } else {
@@ -157,12 +139,86 @@ class TtsService : LifecycleService() {
     }
 
     /**
+     * Read the book using TTS engine.
+     *
+     * @param title Title of the file.
+     * @param pagedBook Contents.
+     * @param currentPage Current page to read.
+     */
+    fun read(title: CharSequence, pagedBook: LinkedList<Spanned>, currentPage: MutableLiveData<Int>) {
+        this.title = title
+        this.pagedBook = pagedBook
+        this.currentPage = currentPage
+
+        // If isTtsRead is false,
+        // then after finish initialize TTS engine, readBook function is triggered automatically.
+        if (isTtsReady) {
+            readBook()
+        }
+    }
+
+    /**
+     * Read book contents.
+     */
+    private fun readBook() {
+        setListenerToReadNextPage()
+
+        updateNotification()
+        textToSpeech.speak(pagedBook[currentPage.value ?: 0], TextToSpeech.QUEUE_FLUSH, null, idUtterance)
+    }
+
+    /**
+     * Read next page when onDone.
+     */
+    private fun setListenerToReadNextPage() {
+        textToSpeech.setOnUtteranceProgressListener(object: UtteranceProgressListener() {
+            override fun onDone(id: String?) {
+                if (currentPage.value == null) { stopSelf() }
+
+                currentPage.value?.let { page ->
+                    updateNotification()
+                    // Go to next page.
+                    if (page + 1 in pagedBook.indices) {
+                        // This is called on worker thread.
+                        currentPage.postValue(currentPage.value?.plus(1))
+                        textToSpeech.speak(pagedBook[page + 1], TextToSpeech.QUEUE_FLUSH, null, idUtterance)
+                    }
+                    else {
+                        stopSelf()
+                    }
+                }
+            }
+
+            override fun onError(id: String?) { stopSelf() }
+            override fun onStart(id: String?) {}
+        })
+    }
+
+    /**
+     * Update the notification message with book contents.
+     */
+    private fun updateNotification() {
+        notification
+            .setContentTitle(title)
+            .setContentText(pagedBook[currentPage.value ?: 0])
+        val notificationManager: NotificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(notificationId, notification.build())
+    }
+
+    /**
      * Shutdown the TTS engine.
      */
     private fun stopRead() {
-        textToSpeech.stop()
-        textToSpeech.shutdown()
+        if (isTtsReady) {
+            textToSpeech.stop()
+            textToSpeech.shutdown()
+            isTtsReady = false
+        }
     }
+
+    // Return true when all data is initialized.
+    private fun isDataSet() = ::title.isInitialized && ::pagedBook.isInitialized && ::currentPage.isInitialized
 
     /**
      * Local binder to bind service with ViewModel.
