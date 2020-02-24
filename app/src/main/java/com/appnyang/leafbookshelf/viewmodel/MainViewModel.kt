b2b27764a304bcc705c1c0eaf6395677bac0b525
Sangwoo Park <eicpein@gmail.com>
2020-02-24 19:01:47 +0900

    Change history on MainViewModel to MediatorLiveData
    It need to an empty live data to prevent
    'Failed to call observer method'.

--- app/src/main/java/com/appnyang/leafbookshelf/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/appnyang/leafbookshelf/viewmodel/MainViewModel.kt
@@ -1,16 +1,25 @@
 package com.appnyang.leafbookshelf.viewmodel
 
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.appnyang.leafbookshelf.data.model.history.History
 import com.appnyang.leafbookshelf.data.repository.BookmarkRepository
 import com.appnyang.leafbookshelf.data.repository.HistoryRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * Main View Model.
  *
  * @author Sangwoo <sangwoo@yesang.com> on 2020-01-20.
  */
-class MainViewModel(historyRepo: HistoryRepository, bookmarkRepo: BookmarkRepository) : ViewModel() {
+class MainViewModel(historyRepo: HistoryRepository, private val bookmarkRepo: BookmarkRepository) : ViewModel() {
 
-    val history = historyRepo.loadHistory()
+    val history = MediatorLiveData<List<History>>()
     val bookmarks = bookmarkRepo.loadBookmarks()
+
+    init {
+        // This empty live data prevents 'Failed to call observer method' error.
+        history.addSource(MutableLiveData<List<History>>(listOf())) { history.value = it }
+        history.addSource(historyRepo.loadHistory()) { history.value = it }
+    }
 }
