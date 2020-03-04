1c274f895b10439db698e0135e19fa9576dbfef5
Sangwoo Park <eicpein@gmail.com>
2020-03-04 21:17:06 +0900

    Create PreferenceActivity

---
+++ app/src/main/java/com/appnyang/leafbookshelf/view/preference/activity/PreferenceActivity.kt
@@ -0,0 +1,20 @@
+package com.appnyang.leafbookshelf.view.preference.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.appnyang.leafbookshelf.R
+import com.appnyang.leafbookshelf.view.preference.fragment.PreferenceFragment
+import kotlinx.android.synthetic.main.activity_preference.*
+
+class PreferenceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preference)
+
+        setSupportActionBar(toolBar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
