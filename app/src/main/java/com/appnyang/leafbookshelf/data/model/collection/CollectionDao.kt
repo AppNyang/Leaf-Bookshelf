5a735596fa203bb2ccde5c55eb3808998eb5727c
Sangwoo Park <eicpein@gmail.com>
2020-04-29 00:01:28 +0900

    Add CRUD functions to CollectionDao

--- app/src/main/java/com/appnyang/leafbookshelf/data/model/collection/CollectionDao.kt
+++ app/src/main/java/com/appnyang/leafbookshelf/data/model/collection/CollectionDao.kt
@@ -18,8 +18,17 @@ interface CollectionDao {
     @Query("SELECT * FROM collections")
     fun getCollections(): LiveData<List<Collection>>
 
+    @Query("SELECT * FROM collections WHERE id = :id")
+    fun getCollection(id: Long): LiveData<Collection>
+
     @Query("SELECT books FROM collections WHERE id = :id")
     fun getBooks(id: Long): LiveData<List<String>>
+
+    @Update
+    fun update(collection: Collection)
+
+    @Delete
+    fun delete(collection: Collection)
 
     @Query("DELETE FROM collections WHERE id = :id")
     fun delete(id: Long)
