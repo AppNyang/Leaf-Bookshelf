2121b41e798b685717726d1373716bf3afe66218
Sangwoo Park <eicpein@gmail.com>
2020-04-29 00:14:19 +0900

    Fix NPE of CollectionBindingAdapter
    The items parameter would be null before featching.

--- app/src/main/java/com/appnyang/leafbookshelf/view/main/CollectionBindingAdapter.kt
+++ app/src/main/java/com/appnyang/leafbookshelf/view/main/CollectionBindingAdapter.kt
@@ -13,22 +13,28 @@ import com.appnyang.leafbookshelf.util.MarginDecoration
  * @author Sangwoo <sangwoo@yesang.com> on 2020-04-25.
  */
 @BindingAdapter("collections")
-fun setCollections(view: RecyclerView, items: List<Collection>) {
-    view.adapter?.let {
-        if (it is CollectionAdapter) {
-            it.items = items
-            it.notifyDataSetChanged()
+fun setCollections(view: RecyclerView, items: List<Collection>?) {
+    if (items != null) {
+        view.adapter?.let {
+            if (it is CollectionAdapter) {
+                it.items = items
+                it.notifyDataSetChanged()
+            }
+        } ?: run {
+            view.setHasFixedSize(true)
+
+            LinearSnapHelper().attachToRecyclerView(view)
+
+            // Set margin decoration.
+            val marginHorizontal = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                8f,
+                view.resources.displayMetrics
+            ).toInt()
+            view.addItemDecoration(MarginDecoration(0, 0, marginHorizontal, marginHorizontal))
+
+            // Create an adapter because view.adapter is null.
+            view.adapter = CollectionAdapter(items)
         }
-    } ?: run {
-        view.setHasFixedSize(true)
-
-        LinearSnapHelper().attachToRecyclerView(view)
-
-        // Set margin decoration.
-        val marginHorizontal = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8f, view.resources.displayMetrics).toInt()
-        view.addItemDecoration(MarginDecoration(0, 0, marginHorizontal, marginHorizontal))
-
-        // Create an adapter because view.adapter is null.
-        view.adapter = CollectionAdapter(items)
     }
 }
