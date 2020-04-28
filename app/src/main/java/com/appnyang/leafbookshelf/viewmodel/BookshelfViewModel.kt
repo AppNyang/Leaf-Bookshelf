1e57d5f28960d08ed109ffd3ae0ab575ff8da1f0
Sangwoo Park <eicpein@gmail.com>
2020-04-29 00:12:23 +0900

    Inject CollectionRepo to BookshelfViewModel

--- app/src/main/java/com/appnyang/leafbookshelf/viewmodel/BookshelfViewModel.kt
+++ app/src/main/java/com/appnyang/leafbookshelf/viewmodel/BookshelfViewModel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.appnyang.leafbookshelf.data.model.collection.Collection
 import com.appnyang.leafbookshelf.data.repository.BookmarkRepository
+import com.appnyang.leafbookshelf.data.repository.CollectionRepository
 import com.appnyang.leafbookshelf.data.repository.HistoryRepository
 import com.appnyang.leafbookshelf.view.bookshelf.OnHistoryItemClickListener
 import kotlinx.coroutines.Dispatchers
@@ -15,9 +17,14 @@ import kotlinx.coroutines.launch
  *
  * @author Sangwoo <sangwoo@yesang.com> on 2020-03-19.
  */
-class BookshelfViewModel(historyRepo: HistoryRepository, bookmarkRepo: BookmarkRepository) : ViewModel()  {
+class BookshelfViewModel(
+    historyRepo: HistoryRepository,
+    bookmarkRepo: BookmarkRepository,
+    private val collectionRepo: CollectionRepository
+) : ViewModel()  {
 
     val histories = historyRepo.loadHistory()
+    val collections = collectionRepo.loadCollections()
 
     private val _historyClicked = MutableLiveData<Pair<String, Long>>()
     val historyClicked: LiveData<Pair<String, Long>> = _historyClicked
