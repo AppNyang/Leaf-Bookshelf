100522d50bf29af36d11abf1a6cc60f2c16b696a
Sangwoo <coro101@naver.com>
2020-06-19 00:39:34 +0900

    Add to extract shadow color from book cover using Palette

--- app/src/main/java/com/appnyang/leafbookshelf/view/book/CoverBindingAdapter.kt
+++ app/src/main/java/com/appnyang/leafbookshelf/view/book/CoverBindingAdapter.kt
@@ -1,9 +1,17 @@
 package com.appnyang.leafbookshelf.view.book
 
+import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Build
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import androidx.palette.graphics.Palette
 import com.appnyang.leafbookshelf.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.google.android.material.imageview.ShapeableImageView
 
 /**
@@ -23,6 +31,26 @@ fun setCover(view: ShapeableImageView, cover: Uri?) {
                 }
                 else {
                     load(cover)
+                }
+
+                // Pick muted color from palette and set it to shadow color.
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    listener(object: RequestListener<Bitmap> {
+                        override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Bitmap>?, isFirstResource: Boolean) = false
+
+                        override fun onResourceReady(resource: Bitmap?, model: Any?, target: Target<Bitmap>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                            if (resource != null) {
+                                Palette.from(resource).generate { palette ->
+                                    val defaultColor = ContextCompat.getColor(view.context, R.color.darkEmerald)
+                                    palette?.getMutedColor(defaultColor).let { mutedColor ->
+                                        view.outlineAmbientShadowColor = mutedColor ?: defaultColor
+                                        view.outlineSpotShadowColor = mutedColor ?: defaultColor
+                                    }
+                                }
+                            }
+                            return false
+                        }
+                    })
                 }
 
                 // Set image to the view.
